b1c5d9826b6619a34d81de958e4e912789ff7891
Zeynep Sude Duman <zsduman.34@gmail.com>
2024-02-02 20:03:41 +0300

    Hw file is created.

---
+++ hw1.py
@@ -0,0 +1,56 @@
+## LOOP WİTH RANGE ##
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+#Hard Level
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+## letters, symbols, ve numbers listeleri, farklı veri türlerini içerir:
+
+## letters bir karakter (string) listesidir. Bu nedenle her karakteri ayrı bir liste öğesi olarak eklemek için append kullanılır.
+
+## symbols ve numbers ise tek karakterden oluşan string değerlerdir. Bu nedenle bu karakterleri listeye eklemek için += operatörünü kullanabilirsiniz. Python'da bir string, karakterlerin bir dizisi olduğu için += operatörünü kullanarak karakteri listenin sonuna ekleyebilirsiniz.
+
+## Bu nedenle, letters listesini append ile genişletmek ve symbols ile numbers listelerini += operatörü ile genişletmek, karakter türlerine uygun bir şekilde veri eklemenizi sağlar.
+
